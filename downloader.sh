f821af27e13c17100a968d6c0a42525fb75f0055
git4o <71034923+git4o@users.noreply.github.com>
2023-04-06 18:46:31 -0700

    Add files via upload

---
+++ downloader.sh
@@ -0,0 +1,27 @@
+sudo dd if=/dev/zero bs=1M count=1024 of=/mnt/1GiB.swap
+
+sudo chmod 600 /mnt/1GiB.swap
+
+sudo mkswap /mnt/1GiB.swap
+
+sudo swapon /mnt/1GiB.swap
+
+
+
+
+cat /proc/swaps
+
+
+
+
+echo '/mnt/1GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
+
+
+
+
+
+wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.12.0.d4fd8a.tar.gz
+
+
+tar -xf kasm_release*.tar.gz
+
